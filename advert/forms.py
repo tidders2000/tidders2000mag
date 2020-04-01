9642f421178e8b691ed2dab3faf9234978316a1b
Ubuntu <ubuntu@ip-172-31-38-85.ec2.internal>
2020-04-01 15:52:07 +0000

    added message text

--- advert/forms.py
+++ advert/forms.py
@@ -4,5 +4,5 @@ from .models import Advert
 class advert_form(forms.ModelForm):
     class Meta:
          model = Advert
-         fields = ('business_name','info','contact_email','contact_telephone','slug','advert_image',)
+         fields = ('business_name','info','contact_email','contact_telephone','slug','advert_image','website',)
          
